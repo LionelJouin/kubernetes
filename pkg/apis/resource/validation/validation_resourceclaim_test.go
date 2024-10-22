f6d75f41a41609b3adbfc9cd91b0e75920f014e1
Lionel Jouin <lionel.jouin@est.tech>
2024-10-22 18:08:57 +0200

    [KEP-4817] improve testing
    * Test feature-gate enabled/disabled for validation
    * Test pkg/registry/resource/resourceclaim
    * Add Data and NetworkData to integration test
    
    Signed-off-by: Lionel Jouin <lionel.jouin@est.tech>

--- pkg/apis/resource/validation/validation_resourceclaim_test.go
+++ pkg/apis/resource/validation/validation_resourceclaim_test.go
@@ -407,8 +407,6 @@ func TestValidateClaimUpdate(t *testing.T) {
 }
 
 func TestValidateClaimStatusUpdate(t *testing.T) {
-	featuregatetesting.SetFeatureGateDuringTest(t, utilfeature.DefaultFeatureGate, features.DRAResourceClaimDeviceStatus, true)
-
 	validAllocatedClaim := validClaim.DeepCopy()
 	validAllocatedClaim.Status = resource.ResourceClaimStatus{
 		Allocation: &resource.AllocationResult{
@@ -427,10 +425,11 @@ func TestValidateClaimStatusUpdate(t *testing.T) {
 	validAllocatedClaimOld.Status.Allocation.Devices.Results[0].AdminAccess = nil // Not required in 1.31.
 
 	scenarios := map[string]struct {
-		adminAccess  bool
-		oldClaim     *resource.ResourceClaim
-		update       func(claim *resource.ResourceClaim) *resource.ResourceClaim
-		wantFailures field.ErrorList
+		adminAccess             bool
+		deviceStatusFeatureGate bool
+		oldClaim                *resource.ResourceClaim
+		update                  func(claim *resource.ResourceClaim) *resource.ResourceClaim
+		wantFailures            field.ErrorList
 	}{
 		"valid-no-op-update": {
 			oldClaim: validClaim,
@@ -722,6 +721,7 @@ func TestValidateClaimStatusUpdate(t *testing.T) {
 				}
 				return claim
 			},
+			deviceStatusFeatureGate: true,
 		},
 		"invalid-device-status-duplicate": {
 			wantFailures: field.ErrorList{
@@ -744,6 +744,7 @@ func TestValidateClaimStatusUpdate(t *testing.T) {
 				}
 				return claim
 			},
+			deviceStatusFeatureGate: true,
 		},
 		"invalid-network-device-status": {
 			wantFailures: field.ErrorList{
@@ -766,6 +767,7 @@ func TestValidateClaimStatusUpdate(t *testing.T) {
 				}
 				return claim
 			},
+			deviceStatusFeatureGate: true,
 		},
 		"invalid-data-device-status": {
 			wantFailures: field.ErrorList{
@@ -786,6 +788,7 @@ func TestValidateClaimStatusUpdate(t *testing.T) {
 				}
 				return claim
 			},
+			deviceStatusFeatureGate: true,
 		},
 		"invalid-device-status-no-device": {
 			wantFailures: field.ErrorList{
@@ -803,12 +806,92 @@ func TestValidateClaimStatusUpdate(t *testing.T) {
 				}
 				return claim
 			},
+			deviceStatusFeatureGate: true,
+		},
+		"invalid-device-status-duplicate-disabled-feature-gate": {
+			wantFailures: nil,
+			oldClaim:     func() *resource.ResourceClaim { return validAllocatedClaim }(),
+			update: func(claim *resource.ResourceClaim) *resource.ResourceClaim {
+				claim = claim.DeepCopy()
+				claim.Status.Devices = []resource.AllocatedDeviceStatus{
+					{
+						Driver: goodName,
+						Pool:   goodName,
+						Device: goodName,
+					},
+					{
+						Driver: goodName,
+						Pool:   goodName,
+						Device: goodName,
+					},
+				}
+				return claim
+			},
+			deviceStatusFeatureGate: false,
+		},
+		"invalid-network-device-status-disabled-feature-gate": {
+			wantFailures: nil,
+			oldClaim:     func() *resource.ResourceClaim { return validAllocatedClaim }(),
+			update: func(claim *resource.ResourceClaim) *resource.ResourceClaim {
+				claim = claim.DeepCopy()
+				claim.Status.Devices = []resource.AllocatedDeviceStatus{
+					{
+						Driver: goodName,
+						Pool:   goodName,
+						Device: goodName,
+						NetworkData: &resource.NetworkDeviceData{
+							Addresses: []string{
+								"300.9.8.0/24",
+							},
+						},
+					},
+				}
+				return claim
+			},
+			deviceStatusFeatureGate: false,
+		},
+		"invalid-data-device-status-disabled-feature-gate": {
+			wantFailures: nil,
+			oldClaim:     func() *resource.ResourceClaim { return validAllocatedClaim }(),
+			update: func(claim *resource.ResourceClaim) *resource.ResourceClaim {
+				claim = claim.DeepCopy()
+				claim.Status.Devices = []resource.AllocatedDeviceStatus{
+					{
+						Driver: goodName,
+						Pool:   goodName,
+						Device: goodName,
+						Data: &runtime.RawExtension{
+							Raw: []byte(`foo`),
+						},
+					},
+				}
+				return claim
+			},
+			deviceStatusFeatureGate: false,
+		},
+		"invalid-device-status-no-device-disabled-feature-gate": {
+			wantFailures: nil,
+			oldClaim:     func() *resource.ResourceClaim { return validAllocatedClaim }(),
+			update: func(claim *resource.ResourceClaim) *resource.ResourceClaim {
+				claim = claim.DeepCopy()
+				claim.Status.Devices = []resource.AllocatedDeviceStatus{
+					{
+						Driver: "b",
+						Pool:   "a",
+						Device: "r",
+					},
+				}
+				return claim
+			},
+			deviceStatusFeatureGate: false,
 		},
 	}
 
 	for name, scenario := range scenarios {
 		t.Run(name, func(t *testing.T) {
 			featuregatetesting.SetFeatureGateDuringTest(t, utilfeature.DefaultFeatureGate, features.DRAAdminAccess, scenario.adminAccess)
+			featuregatetesting.SetFeatureGateDuringTest(t, utilfeature.DefaultFeatureGate, features.DRAResourceClaimDeviceStatus, scenario.deviceStatusFeatureGate)
+
 			scenario.oldClaim.ResourceVersion = "1"
 			errs := ValidateResourceClaimStatusUpdate(scenario.update(scenario.oldClaim.DeepCopy()), scenario.oldClaim)
 			assert.Equal(t, scenario.wantFailures, errs)
