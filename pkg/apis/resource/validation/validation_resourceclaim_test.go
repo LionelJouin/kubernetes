ba5d87bc27789ac44b8fa48bdc2f5a704abb4020
Lionel Jouin <lionel.jouin@est.tech>
2024-10-28 14:41:18 +0100

    [KEP-4817] Remove pointer on Data, InterfaceName and HWAddress fields
    Adapat validation and tests based on these API changes
    
    Signed-off-by: Lionel Jouin <lionel.jouin@est.tech>

--- pkg/apis/resource/validation/validation_resourceclaim_test.go
+++ pkg/apis/resource/validation/validation_resourceclaim_test.go
@@ -706,16 +706,16 @@ func TestValidateClaimStatusUpdate(t *testing.T) {
 								Status: metav1.ConditionTrue,
 							},
 						},
-						Data: &runtime.RawExtension{
+						Data: runtime.RawExtension{
 							Raw: []byte(`{"kind": "foo", "apiVersion": "dra.example.com/v1"}`),
 						},
 						NetworkData: &resource.NetworkDeviceData{
-							InterfaceName: ptr.To("net-1"),
+							InterfaceName: "net-1",
 							Addresses: []string{
 								"10.9.8.0/24",
 								"2001:db8::/64",
 							},
-							HWAddress: ptr.To("ea:9f:cb:40:b1:7b"),
+							HWAddress: "ea:9f:cb:40:b1:7b",
 						},
 					},
 				}
@@ -781,7 +781,7 @@ func TestValidateClaimStatusUpdate(t *testing.T) {
 						Driver: goodName,
 						Pool:   goodName,
 						Device: goodName,
-						Data: &runtime.RawExtension{
+						Data: runtime.RawExtension{
 							Raw: []byte(`foo`),
 						},
 					},
@@ -860,7 +860,7 @@ func TestValidateClaimStatusUpdate(t *testing.T) {
 						Driver: goodName,
 						Pool:   goodName,
 						Device: goodName,
-						Data: &runtime.RawExtension{
+						Data: runtime.RawExtension{
 							Raw: []byte(`foo`),
 						},
 					},
